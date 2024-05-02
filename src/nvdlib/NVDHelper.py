bd51d4d7d1d34ccc0193262133083c65891e9738
Lorenzo Colelli <lorenzo.colelli@protonmail.com>
2024-05-02 12:20:59 +0200

    initial commit

---
+++ src/nvdlib/NVDHelper.py
@@ -0,0 +1,102 @@
+import sys
+sys.path.append('src')
+import logging
+logging.basicConfig(stream = sys.stderr, level = logging.DEBUG)
+from datetime import datetime
+from utils.Utils import save_to_json_file, get_json_from_file
+from pprint import pprint
+import requests
+import lzma
+import json
+
+
+def __get_json_data_from_xz(url: str):
+    """
+        Desc:
+            Method to retrieve data in .json.xz format from a given url, decompress it and return it in json format
+        Params:
+            :param url: the url to fetch data from
+    """
+    response = requests.get(url)
+    decompressed_data = lzma.decompress(response.content)
+    return json.loads(decompressed_data.decode('utf-8'))
+
+
+def __get_modified_cve_years() -> set:
+    """
+        Desc:
+            Method to get modified CVEs up to the last 8 days. The data is pulled from the following repository:
+            https://github.com/fkie-cad/nvd-json-data-feeds by fkie-cad\n
+            Only if the current system does not have a record of modified data, or it does not match the latest update
+            data will be automatically updated for every CVE year included in the modified json
+        Returns:
+            set of the modified CVE years
+    """
+    try:
+        last_modified = get_json_from_file("CVE-Modified.json")
+    except FileNotFoundError:
+        last_modified = None
+
+    request_link = "https://github.com/fkie-cad/nvd-json-data-feeds/releases/latest/download/CVE-Modified.json.xz"
+    formatted_data = __get_json_data_from_xz(request_link)
+
+    if last_modified != None and last_modified['timestamp'] == formatted_data['timestamp']:
+        logging.info("Data already up-to-date")
+        return set([])
+    
+    out = []
+    for cve in formatted_data['cve_items']:
+        out.append((str(cve['id']).split('-'))[1])
+    
+    save_to_json_file(formatted_data, "CVE-Modified.json")
+    return set(out)
+
+
+def check_for_updates():
+    """
+        Desc:
+            Method to check for data update. It uses an internal call to get all the modified CVEs up to the last 8 days. 
+            The data is pulled from the following repository:
+            https://github.com/fkie-cad/nvd-json-data-feeds by fkie-cad\n
+            Only if the current system does not have a record of modified data, or it does not match the latest update
+            data will be automatically updated for every CVE year included in the modified json
+    """
+    modified_years = __get_modified_cve_years()
+    if len(modified_years) > 0:
+        [save_one_year_json(int(year)) for year in modified_years]
+        logging.info(f'Data updated for years: {[year for year in modified_years]}')
+
+
+def save_one_year_json(year: int = datetime.now().year):
+    """
+        Desc:
+            This method allows the retrieval (and local save) of a specified year CVE dataset from the following repository:
+            https://github.com/fkie-cad/nvd-json-data-feeds by fkie-cad
+            The data is downloaded in .xz format, extracted and saved to .json in the local /data folder in the format 'CVE-<YEAR>.json'.
+        Params:
+            :param year: The desired year to fetch - current year by default
+        Raises:
+            :raises ValueError: if the selected year is not valid. Must be in the range [1999, datetime.now().year]
+    """
+    if year < 1999 or year > datetime.now().year:
+        raise ValueError('Invalid input value: please insert valid year from 1999 to today.')
+    
+    request_link = f"https://github.com/fkie-cad/nvd-json-data-feeds/releases/latest/download/CVE-{year}.json.xz"
+    formatted_data = __get_json_data_from_xz(request_link)
+      
+    save_to_json_file(formatted_data, f'CVE-{year}.json')
+
+
+def save_all_years_json():
+    """
+        Desc:
+            This method allows the retrieval (and local save) of all available year CVE datasets from the following repository:
+            https://github.com/fkie-cad/nvd-json-data-feeds by fkie-cad
+            The data is downloaded in .xz format, extracted and saved to .json in the local /data folder in the format 'CVE-<YEAR>.json'.
+    """
+    cve_all = {}
+    for year in range(1999, datetime.now().year + 1):
+        save_one_year_json(year)
+
+
+save_all_years_json()
---
+++ src/utils/Utils.py
@@ -0,0 +1,27 @@
+import json
+import inspect
+
+# File utils
+
+
+def get_json_from_file(filename: str, path: str = './src/data/'):
+    with open(f"{path}{filename}", "r") as file:
+        return json.load(file)
+
+
+def save_to_json_file(json_data, filename: str, path: str ="./src/data/"):
+    """
+        Desc:
+            Utility methos to save json data to file.
+        Params:
+            :param json_data: the data to be saved
+            :param filename: the filename including the extension of the file (e.g.: 'file.json')
+            :param path: the save folder path - './src/data/' by default
+    """
+    json_string = json.dumps(json_data, indent=4, default=str)
+    __write_to_file__(json_string, filename, path)
+
+
+def __write_to_file__(file_content, filename: str, path: str):
+    with open(f"{path}{filename}", "w") as outfile:
+        outfile.write(file_content)
